f923b23c6751be3d7f2fd831d0fa5f68c6a21c65
Tharushi Karunarathne <tharushin30@icloud.com>
2025-06-09 10:17:17 +0530

    Update ConnectionData.java

--- src/main/java/com/gaming/util/ConnectionData.java
+++ src/main/java/com/gaming/util/ConnectionData.java
@@ -3,6 +3,6 @@ package com.gaming.util;
 public class ConnectionData {
 	public static final String DATABASE = "gamingsite";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "1501";
+    public static final String PASSWORD = " ";
 
 }
